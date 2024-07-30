3f94086124af80e4199af52bd42e814ce07035fc
whtluca <luca.eferraz@gmail.com>
2024-07-30 10:58:36 -0300

    colocando banco de dados

---
+++ mapasala/DAO/ProfessorDAO.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.Sql;
+using System.Data.SqlClient;
+using model.Entidade;
+
+namespace mapasala.DAO
+{
+    public class ProfessorDAO
+    {
+        private string LinhaConexao = "Server=LS05MPF;Database=AULA_DS;User Id=sa;Password=admsql;";
+        private SqlConnection Conexao;
+        public ProfessorDAO()
+        {
+            Conexao = new SqlConnection(LinhaConexao);
+        }
+        public void Inserir(ProfessoresEntidade professor)
+        {
+            Conexao.Open();
+            string query = "Insert into Professores";
+        }
+
+    }
+}
