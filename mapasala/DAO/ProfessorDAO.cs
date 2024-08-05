f7f6fc7c524f16ba41a0eca4f21d9cdf618f4bc8
whtluca <luca.eferraz@gmail.com>
2024-08-05 13:52:15 -0300

    tem que terminar

--- mapasala/DAO/ProfessorDAO.cs
+++ mapasala/DAO/ProfessorDAO.cs
@@ -11,7 +11,7 @@ namespace mapasala.DAO
 {
     public class ProfessorDAO
     {
-        private string LinhaConexao = "Server=LS05MPF;Database=AULA_DS;User Id=sa;Password=admsql;";
+        private string LinhaConexao = "Server=LS05MPF;Database=AULA_DS;User Id=sa;Password=admsasql;";
         private SqlConnection Conexao;
         public ProfessorDAO()
         {
@@ -20,7 +20,11 @@ namespace mapasala.DAO
         public void Inserir(ProfessoresEntidade professor)
         {
             Conexao.Open();
-            string query = "Insert into Professores";
+            string query = "Insert into Professores (Nome , Apelido) Values (@nome, @apelido) ";
+            SqlCommand comando = new SqlCommand(query, Conexao);
+            SqlParameter parametro1 = new SqlParameter("@nome", professor.Nome);
+            SqlParameter parametro2 = new SqlParameter("@apelido", professor.Apelido);
+            comando
         }
 
     }
